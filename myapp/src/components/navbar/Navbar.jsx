2a6ce41f47ead6f71a240afb3f93ad0719279610
samuel <samueltandoh4u@gmail.com>
2024-07-30 19:47:23 +0000

    new

--- myapp/src/components/navbar/Navbar.jsx
+++ myapp/src/components/navbar/Navbar.jsx
@@ -1,9 +1,52 @@
 import React from 'react'
 import './Navbar.css'
+import KeyboardArrowDownOutlinedIcon from '@mui/icons-material/KeyboardArrowDownOutlined';
+import LanguageOutlinedIcon from '@mui/icons-material/LanguageOutlined';
+import DarkModeOutlinedIcon from '@mui/icons-material/DarkModeOutlined';
+import FullscreenExitOutlinedIcon from '@mui/icons-material/FullscreenExitOutlined';
+import NotificationsNoneOutlinedIcon from '@mui/icons-material/NotificationsNoneOutlined';
+import ChatBubbleOutlineOutlinedIcon from '@mui/icons-material/ChatBubbleOutlineOutlined';
+import ListOutlinedIcon from '@mui/icons-material/ListOutlined';
 
 const Navbar = () => {
   return (
-    <div className='navbar'>Navbar</div>
+    <div className='navbar'>
+      <div className="wrapper">
+        <div className='box'>
+         <input type='sort' placeholder='Sort: Last week' />
+          <KeyboardArrowDownOutlinedIcon className='icon'/>
+        </div>
+        <div className="items">
+          <div className="item">
+            <LanguageOutlinedIcon className='icon'/>
+            English
+          </div>
+          <div className="item">
+            <DarkModeOutlinedIcon className='icon'/>
+          </div>
+          <div className="item">
+            <FullscreenExitOutlinedIcon className='icon'/>
+          </div>
+          <div className="item">
+            <NotificationsNoneOutlinedIcon className='icon'/>
+            <div className="counter">1</div>
+          </div>
+          <div className="item">
+            <ChatBubbleOutlineOutlinedIcon className='icon'/>
+            <div className="counter">2</div>
+          </div>
+          <div className="item">
+            <ListOutlinedIcon className='icon'/>
+          </div>
+          <div className="item">
+            <img src='https://th.bing.com/th/id/OIP.wx5Jt5aVcUAAnBP7hK-4pgAAAA?rs=1&pid=ImgDetMain'
+            alt=''
+            className='avatar'
+            />
+          </div>
+        </div>
+      </div>
+    </div>
   )
 }
 
